be097f180bb88bf7150ea3bc7669ec1795615893
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-11-29 17:57:02 +0100

    Add initial steps for landing page feature

---
+++ features/step_definitions/landing_page_feature_steps.rb
@@ -0,0 +1,16 @@
+
+Given("There is a landing page") do
+  visit '/'
+end
+
+When("I visit the landing page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see a title {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see a link {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
