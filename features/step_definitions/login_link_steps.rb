c740507e93c8e1e92b308da86189312ef24fd1f2
marizaswart <mariza.swart@gmail.com>
2017-11-30 10:15:25 +0100

    Set up for login link testing

---
+++ features/step_definitions/login_link_steps.rb
@@ -0,0 +1,11 @@
+Given("there is a landing page") do
+  visit root_path
+end
+
+When("I click on {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be redirected to the users sign in page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
