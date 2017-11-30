e82c1ad7c05e490774823fc85152d5348115c567
marizaswart <mariza.swart@gmail.com>
2017-11-30 10:21:38 +0100

    Test the link for the login button

--- features/login_link.feature
+++ features/login_link.feature
@@ -5,5 +5,5 @@ Feature: Login link
 
   Scenario: As a user I want to be able to login to my account
     Given there is a landing page
-    When I click on "Login"
+    When I click on "Login" link
     Then I should be redirected to the users sign in page
--- features/step_definitions/login_link_steps.rb
+++ features/step_definitions/login_link_steps.rb
@@ -2,8 +2,8 @@ Given("there is a landing page") do
   visit root_path
 end
 
-When("I click on {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I click on {string} link") do |link|
+  click_link link
 end
 
 Then("I should be redirected to the users sign in page") do
